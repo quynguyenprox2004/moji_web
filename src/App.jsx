402aeef7cea14274a6c7dd1b2d5594507603fa97
Nguyen Xuan Quy <quynguyen.16092004@gmail.com>
2026-05-16 23:25:50 +0700

    Add config-mui-theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+// import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
+
 function App() {
   return (
     <>
       <div>trungquandev</div>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
+
       <Button variant="text">Hello world</Button>
       <Button variant="contained">Hello world</Button>
       <Button variant="outlined">Hello world</Button>
       <br />
       <AccessAlarmIcon />
       <ThreeDRotation />
+      <HomeIcon color="primary" />
     </>
   )
 }
