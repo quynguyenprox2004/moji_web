13a5f83560f8a36a3d304c266a12333942daa515
Nguyen Xuan Quy <quynguyen.16092004@gmail.com>
2026-05-17 10:22:56 +0700

    Build_app_bar_v1

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,14 +1,42 @@
 import Box from '@mui/material/Box'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as MojiIcon } from '../../assets/moji.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined'
+import Profiles from './Menus/Profiles'
+// import theme from '../../theme'
+
 
 function AppBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.light',
       width: '100%',
       height: (theme) => theme.moji.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
-    }}></Box>
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto'
+      // backgroundColor: 'primary.main'
+    }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={MojiIcon} fontSize="small" inheritViewBox sx={{ color: 'primary.main' }} />
+          <Typography variant="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Moji</Typography>
+        </Box>
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Tooltip title="Help">
+          <HelpOutlineOutlinedIcon sx={{ cursor: 'pointer' }} />
+        </Tooltip>
+        <Profiles />
+      </Box>
+    </Box>
   )
 }
 
